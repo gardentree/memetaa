48a663845bcc95d0bac3c574e440c5dcd8590cb1
GardenTree <takahiro.ueki@gmail.com>
2012-12-10 15:06:40 +0900

    View keywords and description

---
+++ memetaa.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        memetaa
+// @namespace   https://github.com/gardentree
+// @include     http://*
+// @include     https://*
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
+// @grant       none
+// @version     1
+// ==/UserScript==
+
+container = $("<div style='position:fixed;left:0;bottom:0;width:100%;z-index:10000;padding:3px;background-color:black;color:white;font-size:9px;' />")
+container.append($('<div/>').append($("meta[name=keywords],meta[name=KEYWORDS]").attr('content')))
+container.append($('<div/>').append($("meta[name=description],meta[name=DESCRIPTION]").attr('content')))
+
+container.appendTo('body')
+
