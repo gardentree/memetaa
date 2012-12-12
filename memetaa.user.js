21b1ebdf0c8f00ecde1b6a8e5c1f3ec7a3e5f92a
GardenTree <takahiro.ueki@gmail.com>
2012-12-12 15:24:21 +0900

    Correct code

--- memetaa.user.js
+++ memetaa.user.js
@@ -7,9 +7,9 @@
 // @grant       none
 // ==/UserScript==
 
-container = $("<div style='position:fixed;left:0;bottom:0;width:100%;z-index:10000;padding:3px;background-color:black;color:white;font-size:9px;' />")
-container.append($('<div/>').append($("meta[name=keywords],meta[name=KEYWORDS]").attr('content')))
-container.append($('<div/>').append($("meta[name=description],meta[name=DESCRIPTION]").attr('content')))
+container = $("<div style='position:fixed;left:0;bottom:0;width:100%;z-index:10000;padding:3px;background-color:black;color:white;font-size:9px;' />");
+container.append($('<div/>').append($("meta[name=keywords],meta[name=KEYWORDS]").attr('content')));
+container.append($('<div/>').append($("meta[name=description],meta[name=DESCRIPTION]").attr('content')));
 
-container.appendTo('body')
+container.appendTo('body');
 
